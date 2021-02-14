8dadc0bfc62d4c2625735ebadef2290bfd594aef
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2021-02-14 21:27:43 +0200

    Grand-plan, comments and README

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// <!-- contact section + socials -->
+
+// <!-- header section -->
+
+// <!-- statistics section -->
+
+// <!-- section ('Our Latest Project')  -->
+
+// <!-- section ('Don't Miss Our...')  --> Formos validacija
+
+// <!-- section ('Recent Blog Post?')  --> -tas pats kaip 'Our Latest Project' ???
+
+// <!-- section ('Contact with Us') --> Formos validacija. Gal viena apjungianti 'Contact with us' ir 'Don't Miss Our...' ?
+
+// <!-- footer section socials ??? -->
